5a1ae59164b09c7400dc5684e8d8fc865959e58e
Erick Barbosa <l.bace040211@itses.edu.mx>
2026-08-14 10:09:42 -0600

    Preparar despliegue: URLs configurables, netlify.toml, ignorar node_modules/.env

--- F_StudyHub/lib/core/constants.dart
+++ F_StudyHub/lib/core/constants.dart
@@ -1,2 +1,2 @@
-const String kApiBaseUrl = 'http://localhost:4000';
-const String kSocketUrl = 'http://localhost:4000';
+const String kApiBaseUrl = String.fromEnvironment('API_URL', defaultValue: 'http://localhost:4000');
+const String kSocketUrl = String.fromEnvironment('SOCKET_URL', defaultValue: 'http://localhost:4000');
